9af4a8043e71c5a7b47905cf4847324117669e52
Stefano Belfiglio <83875374+stefanobelfiglio@users.noreply.github.com>
2023-07-30 14:37:01 +0200

    Login e registrazione inizio

--- include/dbms.inc.php
+++ include/dbms.inc.php
@@ -6,21 +6,21 @@
 
     $config['localhost']['host'] = "localhost";
     $config['localhost']['user'] = "root";
-    $config['localhost']['passwd'] = "viva1felice";
-    $config['localhost']['db_name'] = "tdw_2223";
+    $config['localhost']['passwd'] = "";
+    $config['localhost']['db_name'] = "e_learning";
     
     // deployment architecture 
-
+    /*
     $config['sql.example.com']['host'] = "localhost";
     $config['sql.example.com']['user'] = "root";
     $config['sql.example.com']['passwd'] = "viva1felice";
     $config['sql.example.com']['db_name'] = "tdw_2223";
-
+*/
     $mysql = new mysqli(
-        $config[$_SERVER["SERVER_NAME"]]['host'],
-        $config[$_SERVER["SERVER_NAME"]]['user'],
-        $config[$_SERVER["SERVER_NAME"]]['passwd'],
-        $config[$_SERVER["SERVER_NAME"]]['db_name']
+        $config[/*$_SERVER["SERVER_NAME"]*/ 'localhost']['host'],
+        $config[/*$_SERVER["SERVER_NAME"]*/ 'localhost']['user'],
+        $config[/*$_SERVER["SERVER_NAME"]*/ 'localhost']['passwd'],
+        $config[/*$_SERVER["SERVER_NAME"]*/ 'localhost']['db_name']
     );
 
     if (!$mysql) {
