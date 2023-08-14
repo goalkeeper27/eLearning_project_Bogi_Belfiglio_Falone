06098b32dc5f9453799115274a9af0ea860c7512
Stefano Belfiglio <83875374+stefanobelfiglio@users.noreply.github.com>
2023-08-14 14:12:05 +0200

    correzione istruttore

--- php/instructor/header_instructor.php
+++ php/instructor/header_instructor.php
@@ -1,12 +1,7 @@
 <?php
-$id_instructor = $_POST['id_instructor'];
 
 #definiamo l'header della pagina del corso specifico
 $header = new Template("skins/revision/dtml/instructor/header_instructor.html");
 
-$info_istruttore = $mysql->query("SELECT * FROM istruttore WHERE ID=$id_instructor");
-while($row = $info_istruttore->fetch_assoc()){
-    $header->setContent('instructor_name',ucfirst(strtolower($row["nome"])).' '.ucfirst(strtolower($row["cognome"])));
-}
 
 ?>
